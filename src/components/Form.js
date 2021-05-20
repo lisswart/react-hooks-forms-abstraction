41fd107aea2e3660c6f88f469a27e83821c2f9da
lisswart <arnoudlisa@gmail.com>
2021-05-19 22:25:18 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,46 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "John",
+    lastName: "Henry"
+  });
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+  // function handleFirstNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     firstName: event.target.value
+  //   });
+  // }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+  // function handleLastNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     lastName: event.target.value
+  //   });
+  // }
+
+function handleChange(event) {
+  // name is the KEY in the formData object we're trying to update
+  const name = event.target.name;
+  const value = event.target.value;
+
+  setFormData({
+    ...formData,
+    [name]: value
+  });
+}
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="text" 
+        name="firstName"
+        onChange={handleChange} 
+        value={formData.firstName} />
+      <input type="text" 
+        name="lastName"
+        onChange={handleChange} 
+        value={formData.lastName} />
       <button type="submit">Submit</button>
     </form>
   );
